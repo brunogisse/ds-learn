6d3f4d67965098524fa8c648d429879e87b413fe
Bruno Gisse <brunogisseju@outlook.com>
2023-01-27 15:06:42 -0300

    Exceptions, validations, security

--- backend/src/main/java/com/brunogisse/dslearn/repositories/UserRepository.java
+++ backend/src/main/java/com/brunogisse/dslearn/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brunogisse.dslearn.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
 
 }
